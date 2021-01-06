a1d9008b826f382d756d8493151a14f59ba953ac
Louis Hobson <louis-hobson@hotmail.co.uk>
2021-01-06 00:55:19 +0000

    Added test source

---
+++ test.cpp
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2020 Louis Hobson <louis-hobson@hotmail.co.uk>. All Rights Reserved.
+ * 
+ * Distributed under MIT licence as a part of the Chess C++ library.
+ * For details, see: https://github.com/louishobson/Chess/blob/master/LICENSE
+ * 
+ * test.cpp
+ * 
+ * test source for Chess
+ * 
+ */
+
+
+
+/* INCLUDES */
+
+#include <iostream>
+
+#include <chess/bitboard.h>
+
+
+/* MAIN */
+
+int main ()
+{
+    std::cout << "Hello, world!" << std::endl;
+
+    return 0;
+}
