52eada268e4c9685614b2b38515bc0150bbe2213
Louis Hobson <louis-hobson@hotmail.co.uk>
2021-01-09 02:55:02 +0000

    Implementation of flipping and rotation

--- src/chess/bitboard.hpp
+++ src/chess/bitboard.hpp
@@ -13,8 +13,8 @@
 
 
 /* HEADER GUARD */
-#ifndef BITBOARD_HPP_INCLUDED
-#define BITBOARD_HPP_INCLUDED
+#ifndef CHESS_BITBOARD_HPP_INCLUDED
+#define CHESS_BITBOARD_HPP_INCLUDED
 
 
 
@@ -23,5 +23,81 @@
 
 
 
+/** @name  vertical_flip
+ * 
+ * @brief  flip the board vertically
+ * @return a new bitboard
+ */
+inline constexpr chess::bitboard chess::bitboard::vertical_flip () const noexcept 
+{
+    /* use builtin if availible, otherwise compute manually */
+#ifdef CHESS_BUILTIN_BSWAP64
+    return bitboard { CHESS_BUILTIN_BSWAP64 ( bits ) }; 
+#else
+    constexpr unsigned long long k1 = 0x00FF00FF00FF00FF, k2 = 0x0000FFFF0000FFFF;
+    unsigned long long x = bits;
+    x = ( ( x >>  8 ) & k1 ) | ( ( x & k1 ) <<  8 );
+    x = ( ( x >> 16 ) & k2 ) | ( ( x & k2 ) << 16 );
+    x = ( ( x >> 32 )      ) | ( ( x      ) << 32 );
+    return bitboard { x };
+#endif
+}
+
+/** @name  horizontal_flip
+ * 
+ * @brief  flip the board horizontally
+ * @return a new bitboard
+ */
+inline constexpr chess::bitboard chess::bitboard::horizontal_flip () const noexcept
+{
+    /* manually compute using rotations */
+    constexpr unsigned long long k1 = 0x5555555555555555, k2 = 0x3333333333333333, k4 = 0x0F0F0F0F0F0F0F0F;
+    unsigned long long x = bits;
+    x ^= k4 & ( x ^ std::rotl ( x, 8 ) );
+    x ^= k2 & ( x ^ std::rotl ( x, 4 ) );
+    x ^= k1 & ( x ^ std::rotl ( x, 2 ) );
+    return bitboard { std::rotr ( x, 7 ) };
+}
+
+/** @name  pos_diag_flip
+ * 
+ * @brief  flip the board along y=x
+ * @return a new bitboard
+ */
+inline constexpr chess::bitboard chess::bitboard::pos_diag_flip () const noexcept
+{
+    /* manually compute */
+    constexpr unsigned long long k1 = 0x5500550055005500, k2 = 0x3333000033330000, k4 = 0x0F0F0F0F00000000;
+    unsigned long long x = bits, t;
+    t  = k4 & ( x ^ ( x << 28 ) );
+    x ^=      ( t ^ ( t >> 28 ) );
+    t  = k2 & ( x ^ ( x << 14 ) );
+    x ^=      ( t ^ ( t >> 14 ) );
+    t  = k1 & ( x ^ ( x <<  7 ) );
+    x ^=      ( t ^ ( t >>  7 ) );
+    return bitboard { x };
+}
+
+/** @name  neg_diag_flip
+ * 
+ * @brief  flip the board along y=-x
+ * @return a new bitboard
+ */
+inline constexpr chess::bitboard chess::bitboard::neg_diag_flip () const noexcept
+{
+    /* manually compute */
+    constexpr unsigned long long k1 = 0xAA00AA00AA00AA00, k2 = 0xCCCC0000CCCC0000, k4 = 0xF0F0F0F00F0F0F0F;
+    unsigned long long x = bits, t;
+    t  =      ( x ^ ( x << 36 ) );
+    x ^= k4 & ( t ^ ( x >> 36 ) );
+    t  = k2 & ( x ^ ( x << 18 ) );
+    x ^=      ( t ^ ( t >> 18 ) );
+    t  = k1 & ( x ^ ( x <<  9 ) );
+    x ^=      ( t ^ ( t >>  9 ) );
+    return bitboard { x };
+}
+
+
+
 /* HEADER GUARD */
-#endif /* #ifndef BITBOARD_HPP_INCLUDED */
+#endif /* #ifndef CHESS_BITBOARD_HPP_INCLUDED */
