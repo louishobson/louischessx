6cb5f5093802170cb24298cf4957951e5521a904
Louis Hobson <louis-hobson@hotmail.co.uk>
2021-03-26 20:17:09 +0000

    Fide deserialization can now handle a '=' or '/' before a promotion type

--- src/chess/chessboard_format.cpp
+++ src/chess/chessboard_format.cpp
@@ -192,7 +192,7 @@ chess::move_t chess::chessboard::fide_deserialize_move ( const pcolor pc, const 
     move_t move { pc };
 
     /* Create the regex to extract the information */
-    std::regex move_regex { "^" "([PNBRQK]?)" "([a-h]?(?=.*?[a-h][1-8]))" "([1-8]?(?=.*?[a-h][1-8]))" "(x?)" "([a-h][1-8])" "([NBRQ]?)" };
+    std::regex move_regex { "^" "([PNBRQK]?)" "([a-h]?(?=.*?[a-h][1-8]))" "([1-8]?(?=.*?[a-h][1-8]))" "(x?)" "([a-h][1-8])" "[=/]?([NBRQ]?)" };
     
     /* Run the search */
     std::smatch move_match; std::regex_search ( desc, move_match, move_regex );
